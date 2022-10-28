3ad7f786ab43d9da96e95a292d58bf2f354efc44
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-28 21:15:16 +0600

    sdf

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("simple Node Server Running");
+});
+
+const users = [
+  { id: 1, name: "sabana", email: "sabana@gmail.com" },
+  { id: 2, name: "sabila", email: "sabila@gmail.com" },
+  { id: 3, name: "shapnur", email: "shapnur@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api called");
 });
 
 app.listen(port, () => {
