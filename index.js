0ebbefc704081f268cdec9fc4661cb4aa476e39d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-29 11:50:40 +0600

    te

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("simple Node Server Running");
@@ -16,11 +17,23 @@ const users = [
 ];
 
 app.get("/users", (req, res) => {
+  if (req.query.name) {
+    const search = req.query.name;
+    const filtered = users.filter(
+      (usr) => usr.name.toLocaleLowerCase().indexOf(search),
+      res.send(filtered)
+    );
+  }
   res.send(users);
 });
 
 app.post("/users", (req, res) => {
   console.log("post api called");
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  console.log(user);
+  res.send(user);
 });
 
 app.listen(port, () => {
